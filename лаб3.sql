367db107945348bda689239f9d5b465c34ebeff2
Yura Chicalo <47437645+YuraChicalo@users.noreply.github.com>
2020-12-23 21:10:35 +0200

    Add files via upload

---
+++ лаб3.sql
@@ -0,0 +1,13 @@
+﻿USE[db_mylibrary1]
+GO
+ALTER TABLE Автори
+ADD [кількість нагород] INT NOT NULL;
+
+ALTER TABLE Читачі
+DROP COLUMN Адреса
+
+ALTER TABLE Читачі
+ALTER COLUMN Вік int  NULL;
+
+ALTER TABLE Працівники
+ADD CHECK (Номер<=50 AND Номер>=0)
