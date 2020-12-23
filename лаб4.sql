f70c6f6b97f0a09719c4f4bbbaafceead913fa7f
Yura Chicalo <47437645+YuraChicalo@users.noreply.github.com>
2020-12-23 21:10:56 +0200

    Add files via upload

---
+++ лаб4.sql
@@ -0,0 +1,18 @@
+﻿USE[db_mylibrary1]
+GO
+
+INSERT INTO Працівники
+VALUES (4, 'pavlo', 'dybun', 'pavlo.dub@ukr.net', '380971653228');
+
+INSERT INTO Працівники
+VALUES (5, 'petro', 'sarri', 'petro.sarr@ukr.net', '380502556778'),
+  (6, 'sasha', 'merza', 'sasha.merza@ukr.net', '380976553221'),
+  (7, 'vasyl', 'mehes', 'vasyl.mehes@ukr.net', '380976554889')
+
+UPDATE Працівники
+  SET Імя='nazar'
+  WHERE id='3'
+
+DELETE
+  FROM Працівники
+   WHERE id='6'
